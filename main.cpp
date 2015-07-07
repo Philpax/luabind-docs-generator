fff344d833279c7f5db116bd1a0015c5126ca52d
Philpax <me@philpax.me>
2015-07-07 18:26:24 +1000

    Converted indentation to spaces

--- main.cpp
+++ main.cpp
@@ -15,88 +15,88 @@ using namespace clang::tooling;
 class DefHandler : public MatchFinder::MatchCallback
 {
 public:
-	virtual void run(MatchFinder::MatchResult const& result)
-	{
-		auto stringLiteral = result.Nodes.getNodeAs<StringLiteral>("name");
-		auto method = result.Nodes.getNodeAs<CXXMethodDecl>("method");
+    virtual void run(MatchFinder::MatchResult const& result)
+    {
+        auto stringLiteral = result.Nodes.getNodeAs<StringLiteral>("name");
+        auto method = result.Nodes.getNodeAs<CXXMethodDecl>("method");
 
-		auto name = stringLiteral->getString().str();
-		auto parentRecord = method->getParent();
+        auto name = stringLiteral->getString().str();
+        auto parentRecord = method->getParent();
 
-		std::cout << "Bound name: " << stringLiteral->getString().str() << "\n";
-		std::cout << "Bound function: " << method->getNameAsString() << "\n";
-		std::cout << "\tParent: " << parentRecord->getNameAsString() << "\n";
-		std::cout << "\tReturn: " << method->getReturnType().getAsString() << "\n";
-		for (auto param : method->params())
-		{
-			std::cout	<< "\tParameter: " << param->getType().getAsString() 
-						<< " " << param->getNameAsString()
-						<< "\n";
-		}
-	}
+        std::cout << "Bound name: " << stringLiteral->getString().str() << "\n";
+        std::cout << "Bound function: " << method->getNameAsString() << "\n";
+        std::cout << "\tParent: " << parentRecord->getNameAsString() << "\n";
+        std::cout << "\tReturn: " << method->getReturnType().getAsString() << "\n";
+        for (auto param : method->params())
+        {
+            std::cout   << "\tParameter: " << param->getType().getAsString() 
+                        << " " << param->getNameAsString()
+                        << "\n";
+        }
+    }
 };
 
 int main(int argc, const char** argv)
 {
-	llvm::cl::OptionCategory luabindDocsGeneratorCategory("Luabind Docs Generator");
+    llvm::cl::OptionCategory luabindDocsGeneratorCategory("Luabind Docs Generator");
     CommonOptionsParser op(argc, argv, luabindDocsGeneratorCategory);
     ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
-	// match lb::class_<*>("*").def("name", &C::F)
-	auto defMatcher = 
-		memberCallExpr
-		(
-			// Match class_ member calls
-			on
-			(
-				hasType
-				(
-					recordDecl
-					(
-						hasName("class_")
-					)
-				)
-			),
-			// Match methods with "def"
-			callee
-			(
-				methodDecl
-				(
-					hasName("def")
-				)
-			),
-			// Match "name"
-			hasArgument
-			(
-				0,
-				ignoringImpCasts
-				(
-					stringLiteral().bind("name")
-				)
-			),
-			// Match &C::F
-			hasArgument
-			(
-				1,
-				unaryOperator
-				(
-					hasUnaryOperand
-					(
-						declRefExpr
-						(
-							hasDeclaration
-							(
-								methodDecl().bind("method")
-							)
-						)
-					)
-				)
-			)
-		).bind("def");
+    // match lb::class_<*>("*").def("name", &C::F)
+    auto defMatcher = 
+        memberCallExpr
+        (
+            // Match class_ member calls
+            on
+            (
+                hasType
+                (
+                    recordDecl
+                    (
+                        hasName("class_")
+                    )
+                )
+            ),
+            // Match methods with "def"
+            callee
+            (
+                methodDecl
+                (
+                    hasName("def")
+                )
+            ),
+            // Match "name"
+            hasArgument
+            (
+                0,
+                ignoringImpCasts
+                (
+                    stringLiteral().bind("name")
+                )
+            ),
+            // Match &C::F
+            hasArgument
+            (
+                1,
+                unaryOperator
+                (
+                    hasUnaryOperand
+                    (
+                        declRefExpr
+                        (
+                            hasDeclaration
+                            (
+                                methodDecl().bind("method")
+                            )
+                        )
+                    )
+                )
+            )
+        ).bind("def");
 
-	DefHandler defHandler;
-	MatchFinder finder;
-	finder.addMatcher(defMatcher, &defHandler);
+    DefHandler defHandler;
+    MatchFinder finder;
+    finder.addMatcher(defMatcher, &defHandler);
 
-	return Tool.run(newFrontendActionFactory(&finder).get());
+    return Tool.run(newFrontendActionFactory(&finder).get());
 }
